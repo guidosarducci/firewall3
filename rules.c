guidosarducci/firewall3

commit 12a7cf9db1f902f359038d9f52851da83c153407

Add support for DSCP matches and target
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

rules.c
@@ -62,6 +62,9 @@ const struct fw3_option fw3_rule_opts[] = {
 	FW3_OPT("set_mark",            mark,      rule,     set_mark),
 	FW3_OPT("set_xmark",           mark,      rule,     set_xmark),
 
+	FW3_OPT("dscp",                dscp,      rule,     dscp),
+	FW3_OPT("set_dscp",            dscp,      rule,     set_dscp),
+
 	FW3_OPT("target",              target,    rule,     target),
 
 	{ }
@@ -156,19 +159,28 @@ check_rule(struct fw3_state *state, struct fw3_rule *r, struct uci_element *e)
 	    r->target == FW3_FLAG_MARK)
 	{
 		warn_section("rule", r, e, "is set to target MARK but specifies neither "
-		                "'set_mark' nor 'set_xmark' option");
+		                           "'set_mark' nor 'set_xmark' option");
 		return false;
 	}
 
-	if (r->_dest && r->target == FW3_FLAG_MARK)
+	if (!r->set_dscp.set && r->target == FW3_FLAG_DSCP)
 	{
-		warn_section("rule", r, e, "must not specify 'dest' for MARK target");
+		warn_section("rule", r, e, "is set to target DSCP but specifies no "
+		                           "'set_dscp' option");
 		return false;
 	}
 
-	if (r->set_mark.invert || r->set_xmark.invert)
+	if (r->_dest && (r->target == FW3_FLAG_MARK || r->target == FW3_FLAG_DSCP))
 	{
-		warn_section("rule", r, e, "must not have inverted 'set_mark' or 'set_xmark'");
+		warn_section("rule", r, e, "must not specify 'dest' for %s target",
+		             fw3_flag_names[r->target]);
+		return false;
+	}
+
+	if (r->set_mark.invert || r->set_xmark.invert || r->set_dscp.invert)
+	{
+		warn_section("rule", r, e, "must not have inverted 'set_mark', "
+		                           "'set_xmark' or 'set_dscp'");
 		return false;
 	}
 
@@ -202,7 +214,7 @@ check_rule(struct fw3_state *state, struct fw3_rule *r, struct uci_element *e)
 		warn_section("rule", r, e, "has no target specified, defaulting to REJECT");
 		r->target = FW3_FLAG_REJECT;
 	}
-	else if (r->target > FW3_FLAG_MARK)
+	else if (r->target > FW3_FLAG_DSCP)
 	{
 		warn_section("rule", r, e, "has invalid target specified, defaulting to REJECT");
 		r->target = FW3_FLAG_REJECT;
@@ -297,7 +309,8 @@ append_chain(struct fw3_ipt_rule *r, struct fw3_rule *rule)
 	{
 		snprintf(chain, sizeof(chain), "zone_%s_helper", rule->src.name);
 	}
-	else if (rule->target == FW3_FLAG_MARK && (rule->_src || rule->src.any))
+	else if ((rule->target == FW3_FLAG_MARK || rule->target == FW3_FLAG_DSCP) &&
+	         (rule->_src || rule->src.any))
 	{
 		snprintf(chain, sizeof(chain), "PREROUTING");
 	}
@@ -351,6 +364,13 @@ static void set_target(struct fw3_ipt_rule *r, struct fw3_rule *rule)
 
 		fw3_ipt_rule_target(r, "MARK");
 		fw3_ipt_rule_addarg(r, false, name, buf);
+		return;
+
+	case FW3_FLAG_DSCP:
+		sprintf(buf, "0x%x", rule->set_dscp.dscp);
+
+		fw3_ipt_rule_target(r, "DSCP");
+		fw3_ipt_rule_addarg(r, false, "--set-dscp", buf);
 		return;
 
 	case FW3_FLAG_NOTRACK:
@@ -461,6 +481,7 @@ print_rule(struct fw3_ipt_handle *handle, struct fw3_state *state,
 	fw3_ipt_rule_limit(r, &rule->limit);
 	fw3_ipt_rule_time(r, &rule->time);
 	fw3_ipt_rule_mark(r, &rule->mark);
+	fw3_ipt_rule_dscp(r, &rule->dscp);
 	set_target(r, rule);
 	fw3_ipt_rule_extra(r, rule->extra);
 	set_comment(r, rule->name, num);
@@ -492,6 +513,7 @@ expand_rule(struct fw3_ipt_handle *handle, struct fw3_state *state,
 	if ((rule->target == FW3_FLAG_NOTRACK && handle->table != FW3_TABLE_RAW) ||
 	    (rule->target == FW3_FLAG_HELPER && handle->table != FW3_TABLE_RAW)  ||
 	    (rule->target == FW3_FLAG_MARK && handle->table != FW3_TABLE_MANGLE) ||
+	    (rule->target == FW3_FLAG_DSCP && handle->table != FW3_TABLE_MANGLE) ||
 		(rule->target < FW3_FLAG_NOTRACK && handle->table != FW3_TABLE_FILTER))
 		return;
 

iptables.c
@@ -1141,6 +1141,20 @@ fw3_ipt_rule_mark(struct fw3_ipt_rule *r, struct fw3_mark *mark)
 }
 
 void
+fw3_ipt_rule_dscp(struct fw3_ipt_rule *r, struct fw3_dscp *dscp)
+{
+	char buf[sizeof("0xFF\0")];
+
+	if (!dscp || !dscp->set)
+		return;
+
+	sprintf(buf, "0x%x", dscp->dscp);
+
+	fw3_ipt_rule_addarg(r, false, "-m", "dscp");
+	fw3_ipt_rule_addarg(r, dscp->invert, "--dscp", buf);
+}
+
+void
 fw3_ipt_rule_comment(struct fw3_ipt_rule *r, const char *fmt, ...)
 {
 	va_list ap;

iptables.h
@@ -93,6 +93,8 @@ void fw3_ipt_rule_time(struct fw3_ipt_rule *r, struct fw3_time *time);
 
 void fw3_ipt_rule_mark(struct fw3_ipt_rule *r, struct fw3_mark *mark);
 
+void fw3_ipt_rule_dscp(struct fw3_ipt_rule *r, struct fw3_dscp *dscp);
+
 void fw3_ipt_rule_comment(struct fw3_ipt_rule *r, const char *fmt, ...);
 
 void fw3_ipt_rule_extra(struct fw3_ipt_rule *r, const char *extra);

options.c
@@ -77,6 +77,7 @@ const char *fw3_flag_names[__FW3_FLAG_MAX] = {
 	"NOTRACK",
 	"HELPER",
 	"MARK",
+	"DSCP",
 	"DNAT",
 	"SNAT",
 	"MASQUERADE",
@@ -133,6 +134,31 @@ static const char *include_types[] = {
 static const char *reflection_sources[] = {
 	"internal",
 	"external",
+};
+
+static const struct { const char *name; uint8_t dscp; } dscp_classes[] = {
+	{ "CS0",  0x00 },
+	{ "CS1",  0x08 },
+	{ "CS2",  0x10 },
+	{ "CS3",  0x18 },
+	{ "CS4",  0x20 },
+	{ "CS5",  0x28 },
+	{ "CS6",  0x30 },
+	{ "CS7",  0x38 },
+	{ "BE",   0x00 },
+	{ "AF11", 0x0a },
+	{ "AF12", 0x0c },
+	{ "AF13", 0x0e },
+	{ "AF21", 0x12 },
+	{ "AF22", 0x14 },
+	{ "AF23", 0x16 },
+	{ "AF31", 0x1a },
+	{ "AF32", 0x1c },
+	{ "AF33", 0x1e },
+	{ "AF41", 0x22 },
+	{ "AF42", 0x24 },
+	{ "AF43", 0x26 },
+	{ "EF",   0x2e }
 };
 
 
@@ -855,6 +881,39 @@ fw3_parse_mark(void *ptr, const char *val, bool is_list)
 	}
 
 	m->set = true;
+	return true;
+}
+
+bool
+fw3_parse_dscp(void *ptr, const char *val, bool is_list)
+{
+	uint32_t n;
+	char *e;
+	struct fw3_dscp *d = ptr;
+
+	if (*val == '!')
+	{
+		d->invert = true;
+		while (isspace(*++val));
+	}
+
+	for (n = 0; n < sizeof(dscp_classes) / sizeof(dscp_classes[0]); n++)
+	{
+		if (strcmp(dscp_classes[n].name, val))
+			continue;
+
+		d->set = true;
+		d->dscp = dscp_classes[n].dscp;
+		return true;
+	}
+
+	n = strtoul(val, &e, 0);
+
+	if (e == val || *e || n > 0x3F)
+		return false;
+
+	d->set = true;
+	d->dscp = n;
 	return true;
 }
 

options.h
@@ -73,17 +73,18 @@ enum fw3_flag
 	FW3_FLAG_NOTRACK       = 9,
 	FW3_FLAG_HELPER        = 10,
 	FW3_FLAG_MARK          = 11,
-	FW3_FLAG_DNAT          = 12,
-	FW3_FLAG_SNAT          = 13,
-	FW3_FLAG_MASQUERADE    = 14,
-	FW3_FLAG_SRC_ACCEPT    = 15,
-	FW3_FLAG_SRC_REJECT    = 16,
-	FW3_FLAG_SRC_DROP      = 17,
-	FW3_FLAG_CUSTOM_CHAINS = 18,
-	FW3_FLAG_SYN_FLOOD     = 19,
-	FW3_FLAG_MTU_FIX       = 20,
-	FW3_FLAG_DROP_INVALID  = 21,
-	FW3_FLAG_HOTPLUG       = 22,
+	FW3_FLAG_DSCP          = 12,
+	FW3_FLAG_DNAT          = 13,
+	FW3_FLAG_SNAT          = 14,
+	FW3_FLAG_MASQUERADE    = 15,
+	FW3_FLAG_SRC_ACCEPT    = 16,
+	FW3_FLAG_SRC_REJECT    = 17,
+	FW3_FLAG_SRC_DROP      = 18,
+	FW3_FLAG_CUSTOM_CHAINS = 19,
+	FW3_FLAG_SYN_FLOOD     = 20,
+	FW3_FLAG_MTU_FIX       = 21,
+	FW3_FLAG_DROP_INVALID  = 22,
+	FW3_FLAG_HOTPLUG       = 23,
 
 	__FW3_FLAG_MAX
 };
@@ -268,6 +269,13 @@ struct fw3_mark
 	uint32_t mask;
 };
 
+struct fw3_dscp
+{
+	bool set;
+	bool invert;
+	uint8_t dscp;
+};
+
 struct fw3_cthelpermatch
 {
 	struct list_head list;
@@ -382,10 +390,12 @@ struct fw3_rule
 	struct fw3_limit limit;
 	struct fw3_time time;
 	struct fw3_mark mark;
+	struct fw3_dscp dscp;
 
 	enum fw3_flag target;
 	struct fw3_mark set_mark;
 	struct fw3_mark set_xmark;
+	struct fw3_dscp set_dscp;
 	struct fw3_cthelpermatch set_helper;
 
 	const char *extra;
@@ -613,6 +623,7 @@ bool fw3_parse_time(void *ptr, const char *val, bool is_list);
 bool fw3_parse_weekdays(void *ptr, const char *val, bool is_list);
 bool fw3_parse_monthdays(void *ptr, const char *val, bool is_list);
 bool fw3_parse_mark(void *ptr, const char *val, bool is_list);
+bool fw3_parse_dscp(void *ptr, const char *val, bool is_list);
 bool fw3_parse_setmatch(void *ptr, const char *val, bool is_list);
 bool fw3_parse_direction(void *ptr, const char *val, bool is_list);
 bool fw3_parse_cthelper(void *ptr, const char *val, bool is_list);

commit f90fa8bb2023ae26041a6d2fc908f5098c032930

rules: fix device and chain usage for DSCP/MARK targets
Currently, fw3 places all DSCP/MARK target rules into the PREROUTING chain,
and accepts but ignores a src device. This behaviour is impractical for
most common applications (e.g. QOS setup), since rules are applied to all
devices and in all directions.

Fix this generally by honouring src/dest device selection and placing the
rules into the appropriate chain of the mangle table. This code is based
on a proof of concept shared by Jo-Philipp Wich <jo@mein.io>.

Testing
=======

Regression testing was done to confirm the default OpenWrt firewall rules
are unchanged before and after this patch.

Functional testing was carried out using the UCI firewall rules below. This
set of test cases would normally yield the following incorrect iptables
rules (from iptables-save, sorted by test):

-A PREROUTING -p udp -m udp --dport 1945 -m comment --comment "!fw3: Test-1-Zone-Any-PRE" -j DSCP --set-dscp 0x00
-A PREROUTING -p tcp -m tcp --dport 1960 -m comment --comment "!fw3: Test-4-Any-Any-FORW" -j MARK --set-xmark 0x7/0xff
-A PREROUTING -p udp -m udp --dport 1965 -m comment --comment "!fw3: Test-5-Zone-Dev-IN" -j DSCP --set-dscp 0x2e
-A OUTPUT -p udp -m udp --dport 1975 -m comment --comment "!fw3: Test-7-Dev-Dev-OUT" -j DSCP --set-dscp 0x30

After this change, the resulting iptables rules use the expected devices
and chains:

-A PREROUTING -i br-lan -p udp -m udp --dport 1945 -m comment --comment "!fw3: Test-1-Zone-Any-PRE" -j DSCP --set-dscp 0x00
-A FORWARD -i br-lan -o eth0 -p udp -m udp --dport 1950 -m comment --comment "!fw3: Test-2-Zone-Zone-FORW" -j DSCP --set-dscp 0x08
-A POSTROUTING -o eth0 -p tcp -m tcp --dport 1955 -m comment --comment "!fw3: Test-3-Any-Zone-POST" -j MARK --set-xmark 0x3/0xff
-A FORWARD -p tcp -m tcp --dport 1960 -m comment --comment "!fw3: Test-4-Any-Any-FORW" -j MARK --set-xmark 0x7/0xff
-A INPUT -i br-lan -p udp -m udp --dport 1965 -m comment --comment "!fw3: Test-5-Zone-Dev-IN" -j DSCP --set-dscp 0x2e
-A OUTPUT -o eth0 -p tcp -m tcp --dport 1970 -m comment --comment "!fw3: Test-6-Dev-Zone-OUT" -j MARK --set-xmark 0xf/0xff
-A OUTPUT -p udp -m udp --dport 1975 -m comment --comment "!fw3: Test-7-Dev-Dev-OUT" -j DSCP --set-dscp 0x30

UCI FW Rules
------------

config rule
        option enabled '1'
        option target 'DSCP'
        option set_dscp 'BE'
        option proto 'udp'
        option dest_port '1945'
        option name 'Test-1-Zone-Any-PRE'
        option src 'lan'
        option dest '*'

config rule
        option enabled '1'
        option target 'DSCP'
        option set_dscp 'CS1'
        option proto 'udp'
        option dest_port '1950'
        option name 'Test-2-Zone-Zone-FORW'
        option src 'lan'
        option dest 'wan'

config rule
        option enabled '1'
        option target 'MARK'
        option set_mark '0x03/0xff'
        option proto 'tcp'
        option dest_port '1955'
        option name 'Test-3-Any-Zone-POST'
        option src '*'
        option dest 'wan'

config rule
        option enabled '1'
        option target 'MARK'
        option set_mark '0x07/0xff'
        option proto 'tcp'
        option dest_port '1960'
        option name 'Test-4-Any-Any-FORW'
        option src '*'
        option dest '*'

config rule
        option enabled '1'
        option target 'DSCP'
        option set_dscp 'EF'
        option proto 'udp'
        option dest_port '1965'
        option name 'Test-5-Zone-Dev-IN'
        option src 'lan'

config rule
        option enabled '1'
        option target 'MARK'
        option set_mark '0x0f/0xff'
        option proto 'tcp'
        option dest_port '1970'
        option name 'Test-6-Dev-Zone-OUT'
        option dest 'wan'

config rule
        option enabled '1'
        option target 'DSCP'
        option set_dscp 'CS6'
        option proto 'udp'
        option dest_port '1975'
        option name 'Test-7-Dev-Dev-OUT'

Fixes: 12a7cf9db1f9 ("Add support for DSCP matches and target")

rules.c
@@ -170,13 +170,6 @@ check_rule(struct fw3_state *state, struct fw3_rule *r, struct uci_element *e)
 		return false;
 	}
 
-	if (r->_dest && (r->target == FW3_FLAG_MARK || r->target == FW3_FLAG_DSCP))
-	{
-		warn_section("rule", r, e, "must not specify 'dest' for %s target",
-		             fw3_flag_names[r->target]);
-		return false;
-	}
-
 	if (r->set_mark.invert || r->set_xmark.invert || r->set_dscp.invert)
 	{
 		warn_section("rule", r, e, "must not have inverted 'set_mark', "
@@ -309,10 +302,19 @@ append_chain(struct fw3_ipt_rule *r, struct fw3_rule *rule)
 	{
 		snprintf(chain, sizeof(chain), "zone_%s_helper", rule->src.name);
 	}
-	else if ((rule->target == FW3_FLAG_MARK || rule->target == FW3_FLAG_DSCP) &&
-	         (rule->_src || rule->src.any))
+	else if (rule->target == FW3_FLAG_MARK || rule->target == FW3_FLAG_DSCP)
 	{
-		snprintf(chain, sizeof(chain), "PREROUTING");
+		if ((rule->_dest && rule->_src) ||
+		    (rule->dest.any && rule->src.any))
+			snprintf(chain, sizeof(chain), "FORWARD");
+		else if (rule->src.any && rule->_dest)
+			snprintf(chain, sizeof(chain), "POSTROUTING");
+		else if (rule->dest.any && rule->_src)
+			snprintf(chain, sizeof(chain), "PREROUTING");
+		else if (!rule->dest.set && rule->src.set)
+			snprintf(chain, sizeof(chain), "INPUT");
+		else /* if (!rule->src.set) */
+			snprintf(chain, sizeof(chain), "OUTPUT");
 	}
 	else
 	{
@@ -420,6 +422,10 @@ print_rule(struct fw3_ipt_handle *handle, struct fw3_state *state,
            struct fw3_mac *mac, struct fw3_icmptype *icmptype)
 {
 	struct fw3_ipt_rule *r;
+	struct fw3_device *idev, *odev;
+	struct list_head empty, *idevices, *odevices;
+	INIT_LIST_HEAD(&empty);
+	idevices = odevices = &empty;
 
 	if (!fw3_is_family(sip, handle->family) ||
 	    !fw3_is_family(dip, handle->family))
@@ -471,21 +477,33 @@ print_rule(struct fw3_ipt_handle *handle, struct fw3_state *state,
 		return;
 	}
 
-	r = fw3_ipt_rule_create(handle, proto, NULL, NULL, sip, dip);
-	fw3_ipt_rule_sport_dport(r, sport, dport);
-	fw3_ipt_rule_device(r, rule->device, rule->direction_out);
-	fw3_ipt_rule_icmptype(r, icmptype);
-	fw3_ipt_rule_mac(r, mac);
-	fw3_ipt_rule_ipset(r, &rule->ipset);
-	fw3_ipt_rule_helper(r, &rule->helper);
-	fw3_ipt_rule_limit(r, &rule->limit);
-	fw3_ipt_rule_time(r, &rule->time);
-	fw3_ipt_rule_mark(r, &rule->mark);
-	fw3_ipt_rule_dscp(r, &rule->dscp);
-	set_target(r, rule);
-	fw3_ipt_rule_extra(r, rule->extra);
-	set_comment(r, rule->name, num);
-	append_chain(r, rule);
+	if (rule->target == FW3_FLAG_DSCP || rule->target == FW3_FLAG_MARK)
+	{
+		if (rule->_src)
+			idevices = &rule->_src->devices;
+		if (rule->_dest)
+			odevices = &rule->_dest->devices;
+	}
+
+	fw3_foreach(idev, idevices)
+	fw3_foreach(odev, odevices)
+	{
+		r = fw3_ipt_rule_create(handle, proto, idev, odev, sip, dip);
+		fw3_ipt_rule_sport_dport(r, sport, dport);
+		fw3_ipt_rule_device(r, rule->device, rule->direction_out);
+		fw3_ipt_rule_icmptype(r, icmptype);
+		fw3_ipt_rule_mac(r, mac);
+		fw3_ipt_rule_ipset(r, &rule->ipset);
+		fw3_ipt_rule_helper(r, &rule->helper);
+		fw3_ipt_rule_limit(r, &rule->limit);
+		fw3_ipt_rule_time(r, &rule->time);
+		fw3_ipt_rule_mark(r, &rule->mark);
+		fw3_ipt_rule_dscp(r, &rule->dscp);
+		set_target(r, rule);
+		fw3_ipt_rule_extra(r, rule->extra);
+		set_comment(r, rule->name, num);
+		append_chain(r, rule);
+	}
 }
 
 static void